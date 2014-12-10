8376c6b3ce16a2e9eabbf1d478d81417de8a84bd
Sylvain Chabert <schabert@arkoon.dev>
2014-12-10 17:41:29 +0100

    Init project backbone

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,20 +1,56 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
 
         // configuration des tâches grunt
 
+        mochaTest: {
+            unit: {
+                options: {
+                    reporter: 'spec'
+                },
+                src: ['test/{,**}/*.js']
+            }
+        },
         mocha_istanbul: {
             coverage: {
                 src: 'test'
             }
+        },
+        watch: {
+            test: {
+                files: [
+                    'src/{,**}/*.js',
+                    'test/{,**}/*.js'
+                ],
+                tasks: ['test']
+            },
+            cucu: {
+                files: [
+                    'features/{,**}/*',
+                    'src/{,**}/*.js'
+                ],
+                tasks: ['cucumberjs']
+            }
+        },
+        cucumberjs: {
+            options: {
+                format: 'summary'
+            },
+            src: 'features/*.feature'
         }
     });
 
-    grunt.loadNpmTasks('grunt-mocha-istanbul')
+    grunt.loadNpmTasks('grunt-mocha-test');
+    grunt.loadNpmTasks('grunt-mocha-istanbul');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-cucumberjs');
 
     // Les tâches sont enregistrées ici
     grunt.registerTask('default', []);
 
+    grunt.registerTask('test', ['mochaTest:unit']);
     grunt.registerTask('coverage', ['mocha_istanbul:coverage']);
+    grunt.registerTask('autotest', ['watch:test']);
+    grunt.registerTask('autocucu', ['watch:cucu']);
 };
--- README.md
+++ README.md
@@ -1,6 +1,37 @@
-Sample Grunt Mocha TDD
+Sample Grunt Mocha TDD/BDD
 ===================
 
-This is a sample project to do TDD (in coding dojo for example) with a javascript project, using grunt and mocha.
+This is a sample project to do TDD/BDD (in coding dojo for example) with a javascript project, using grunt, mocha and cucumber.
 
-There is a coverage task to generate code coverage of tests.
+
+To install
+```
+npm install
+npm install -g grunt-cli
+```
+
+
+To launch tests:
+```
+grunt test
+```
+
+To launch tests automatically after source or test file modification:
+```
+grunt autotest
+```
+
+To launch tests with coverage:
+```
+grunt coverage
+```
+
+To launch tests with cucumber (bdd):
+```
+grunt cucumberjs
+```
+
+To launch cucumber tests automatically after source or test file modification:
+```
+grunt autocucu
+```
